2aaebcbffbe314ac83c5a04b8c0ef0c231a3b247
indusy <teririkawaii@outlook.com>
2022-04-11 19:56:00 +0800

    加入 swagger 以及 antd 依赖

--- src/server/modules/admin/admin.controller.ts
+++ src/server/modules/admin/admin.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get, Render, Req, Res } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('admin')
+@ApiTags("后台管理")
 export class AdminController {
   @Get()
+  @ApiOperation({
+    summary: "admin",
+    description: "admin"
+  })
   @Render("/admin")
   async test(@Req() req, @Res() res) {
     console.log("admin");
